09f08a8de9e6f4f00cff86d8743ca2601696c02d
abinash <mabinashkumar1002@gmail.com>
2025-08-10 22:14:54 +0530

    commiting python changes

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -1,10 +1,10 @@
-with open("test.txt", "w") as f:
-    f.write("write the content")
+with open("testExceptionHandling.txt", "w") as f:
+    f.write("write the content for exception testing")
 
 try:
     with open("test.txt", "r") as f:
         content = f.read()
-        print(content)
+        print("content ====>", content)
 
 except FileNotFoundError as e:
     print(e)
